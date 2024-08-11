63f4f08a30f301e3bdabb2fad31583a9d4fb0894
Ryan McUmber <ryanmma.rm@gmail.com>
2024-08-11 14:02:18 -0400

    goals controller

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,16 @@
-console.log('Hello World');
+const express = require('express')
+const dotenv = require('dotenv').config()
+const {errorHandler} = require('./midleware/errorMiddleware')
+const port = process.env.PORT || 6000
+
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded( {extended : false}))
+
+app.use('/api/goals', require('./routes/goalRoutes'))
+
+app.use(errorHandler)
+
+app.listen(port,()=>console.log(`Sever started on Port ${port}`));
